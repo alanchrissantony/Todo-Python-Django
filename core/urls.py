a998c08ad7e32e1d68fdfa4c42a542178d693afa
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-24 21:05:12 +0530

    todo initial commit

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+
+from core.views import home
+
+
+
+
+
+
+
+
+urlpatterns = [
+    path('', home),
+]
---
+++ core/views.py
@@ -0,0 +1,8 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'home.html')
