ddc6b4624180a91dc73b29da1e4f25aec07e8278
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-26 00:24:41 +0530

    admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Todo
 
 # Register your models here.
+
+admin.site.register(Todo)
--- core/models.py
+++ core/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=256)
+
+
+    def __str__(self):
+        return self.title
